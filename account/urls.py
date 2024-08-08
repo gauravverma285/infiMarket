b8b1c75ff3a4357e5b4bc082ce4d1649e4985aee
Anjalisinghsoft <163495139+Anjalisinghsoft@users.noreply.github.com>
2024-08-08 13:48:25 +0530

    create infimarket project

---
+++ account/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .views import*
+
+urlpatterns = [
+    path('', index, name="index"),
+    path('register/', register, name="register"),
+    path('login/', login, name="login"),
+    path('checkout/', checkout, name="checkout"),
+    path('wishlist/', wishlist, name="wishlist"),
+    path('left_sidebar/', left_sidebar, name="shop-left-sidebar"),
+    path('product-left-sidebar/', product_left_sidebar, name="product-left-sidebar"),
+    path('about/', about, name="about"),
+    path('Contact/', Contact, name="contact-us"),
+    path('cart/', cart, name="cart"),
+    path('track_order/', track_order, name="track-order"),
+    path('FAQ/', FAQ, name="faq"),
+    path('policy/', policy, name="policy"),
+    path('blog_sidebar/', blog_sidebar, name="blog-left-sidebar"),
+    path('elements_product/', elements_product, name="elements-products"),
+]
---
+++ account/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
+def left_sidebar(request):
+    return render(request, 'shop-left-sidebar.html')
+
+def product_left_sidebar(request):
+    return render(request, 'product-left-sidebar.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def Contact(request):
+    return render(request , 'contact-us.html')
+
+def cart(request):
+    return render(request , 'cart.html')
+
+def track_order(request):
+    return render(request , 'track-order.html')
+
+def FAQ(request):
+    return render(request , 'faq.html')
+
+def policy(request):
+    return render(request , 'policy.html')
+
+def blog_sidebar(request):
+    return render(request , 'blog-left-sidebar.html')
+
+def elements_product(request):
+    return render(request , 'elements-products.html')
